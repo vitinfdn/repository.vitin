30ab6a122710da446891007dcc512fb54c0acee3
Victor Ferreira do Nascimento <victor.nascimento@radixeng.com.br>
2022-06-15 23:36:52 -0300

    novo arquivo de conversão de tipos

---
+++ conversoes.js
@@ -0,0 +1,23 @@
+// tipos de dados
+// conversões de tipos
+// booleanos
+
+// conversão implícita
+
+const numero = 456;
+const numeroString = "456";
+
+console.log(typeof numero);
+console.log(typeof numeroString);
+// se === compara-se tipo e o "valor"
+// se == compara-se apenas o "valor" gerando a conversão de tipo
+
+
+
+// conversão explícita
+
+//Number()
+//String ()
+
+console.log(numero + Number(numeroString));
+
