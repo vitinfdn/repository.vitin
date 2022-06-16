2f2fae4fbcf8f13cbf50a69ebbbef372d0cf7fe5
Victor Ferreira do Nascimento <victor.nascimento@radixeng.com.br>
2022-06-15 22:56:20 -0300

    novo código da aula de boolean

---
+++ truthy_false.js
@@ -0,0 +1,12 @@
+//boolean -> true or false
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy or falsy
+// 0 -> false 
+// 1 -> true
+
+console.log(0 == false);
+console.log(0 == false);
+
