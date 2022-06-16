3d0c3f24f6220b9cc0a7d6c7cbe8824e2729108b
Victor Ferreira do Nascimento <victor.nascimento@radixeng.com.br>
2022-06-16 19:41:37 -0300

    Teste de console do curso de Js

--- conversoes.js
+++ conversoes.js
@@ -4,11 +4,11 @@
 
 // conversão implícita
 
-const numero = 456;
-const numeroString = Number ("456a");
+//const numero = 456;
+//const numeroString = Number ("456a");
 
-console.log(typeof numero);
-console.log(typeof numeroString);
+//console.log(typeof numero);
+//console.log(typeof numeroString);
 // se === compara-se tipo e o "valor"
 // se == compara-se apenas o "valor" gerando a conversão de tipo
 
@@ -19,5 +19,19 @@ console.log(typeof numeroString);
 //Number()
 //String ()
 
-console.log(numero + numeroString);
+//console.log(numero + numeroString);
 
+
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+
+console.log(respostaDeTudo, idade, pi)
