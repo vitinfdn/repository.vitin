7eb9242cf64b89ab38112709e91f502a3d99f72b
Victor Ferreira do Nascimento <victor.nascimento@radixeng.com.br>
2022-06-15 22:58:02 -0300

    novo código da aula de boolean

--- truthy_false.js
+++ truthy_false.js
@@ -1,12 +1,33 @@
-//boolean -> true or false
+// boolean -> true or false
 
 const usuarioLogado = true;
 const contaPaga = false;
 
-//truthy or falsy
+// truthy or falsy
 // 0 -> false 
 // 1 -> true
 
-console.log(0 == false);
-console.log(0 == false);
+//console.log(0 == false);
+//console.log(" " == false);
 
+//console.log(1 == true);
+
+// null > vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+//console.log(minhaVar);
+//console.log(varNull);
+
+let numero = 3;
+let texto = "Alura";
+
+// var = null > typeof é um objeto
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+
+
+// undefined
